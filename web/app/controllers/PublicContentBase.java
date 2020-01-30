413b51ed0c517aa065a0e469548ce1410d1f01b6
ignacio <ignacio@url.tld>
2020-01-30 20:00:30 +0100

    Ignacio Garcia-Escribano Martin: Front-end and back-end validation of user role when registering

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -12,9 +12,14 @@ public class PublicContentBase extends Controller {
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+        if (type != null && (type.equals("teacher") || type.equals("student"))) {
+            User u = new User(username, HashUtils.getMd5(password), type, -1);
+            u.save();
+            registerComplete();
+        } else {
+            flash("error", "Invalid role");
+            register();
+        }
     }
 
     public static void registerComplete(){
